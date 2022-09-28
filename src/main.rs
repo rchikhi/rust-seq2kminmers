fba9baec15c6e2d68b1f5f270a68bb1bb10798ab
Rayan  CHIKHI <rchikhi@maestro-submit.maestro.pasteur.fr>
2022-09-28 17:05:44 +0200

    optimized the offending kminmer creation lines, and also, moved the KmimerWrite/Read to old code

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 
-use rust_seq2kminmers::{KminmersIterator, KminmersWriteIterator, KminmersReadIterator};
+use rust_seq2kminmers::{KminmersIterator};
 
 fn main() {
     let seq = b"AACTGCACTGCACTGCACTGCACACTGCACTGCACTGCACTGCACACTGCACTGCACTGACTGCACTGCACTGCACTGCACTGCCTGC";
@@ -7,18 +7,6 @@ fn main() {
     for kminmer in iter
     {
         println!("kminmer: {:?}",kminmer);
-    }
-    println!("Write minimizers while generating k-min-mers:");
-    let iter = KminmersWriteIterator::new(seq, 10, 5, 0.1, false, "seq").unwrap();
-    for kminmer in iter
-    {
-        println!("kminmer-write: {:?}",kminmer);
-    }
-    println!("Read minimizers and generate k-min-mers:");
-    let iter2 = KminmersReadIterator::new(10, 4, 0.1, "seq").unwrap();
-    for kminmer in iter2
-    {
-        println!("kminmer-read: {:?}",kminmer);
     }
 }
 
